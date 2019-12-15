b7c8bf2c72646206b1d4e8f9e82b58bdceec73fb
mitchhymel <mjhymel111@gmail.com>
2019-12-14 22:48:37 -0800

    Initial commit of base structure of client

---
+++ example/example.dart
@@ -0,0 +1,13 @@
+
+import 'package:lastfmapi/lastfm_api.dart';
+
+import 'secret.dart';
+
+var client = new LastFmApi(API_KEY, SHARED_SECRET, 'lastfm_dart_example', 
+  logger: LastFmConsoleLogger());
+
+main() async {
+
+  var result = await client.artistGetSimilar('In Aviate');
+
+}
